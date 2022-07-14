d648050d48eb12e6c51d539b280ec4483734ce5d
= <=>
2022-07-14 10:21:37 +0800

    init

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const { Octokit } = require("@octokit/action")
+const { createActionAuth } = require("@octokit/auth-action");
 
 const run = async () => {
   try {
     const run_id = core.getInput('run-id')
-    const token = core.getInput('repo-token')
     const { repo } = github.context
 
+    const auth = createActionAuth()
+    const authentication = await auth()
 
     const octokit = new Octokit({
-      auth: token
+      auth: authentication.token
     });
 
     const { response } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/attempts/${1}/jobs`);
