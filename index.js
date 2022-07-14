69aeaf2dc487aed4f38a61f3d65129273f49b3d9
= <=>
2022-07-14 09:45:55 +0800

    init

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { Octokit } = require("@octokit/action");
+const core = require('@actions/core')
+const github = require('@actions/github')
+const { Octokit } = require("@octokit/action")
 
 const run = async () => {
   try {
-    const run_id = core.getInput('run-id');
-    console.log(run_id);
-    const {repo} = github.context;
+    const run_id = core.getInput('run-id')
+    const token = core.getInput('repo-token')
+    const { repo } = github.context
 
 
-  
-  
+    const octokit = new Octokit({
+      auth: token
+    });
+
     const { response } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/attempts/${1}/jobs`);
-  
+
     console.log(response)
-  
-  
+
+
   } catch (error) {
     core.setFailed(error.message);
   }
