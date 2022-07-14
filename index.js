728f97150e8a2fad2880d1cc0f5c1e52017bf816
= <=>
2022-07-14 09:18:01 +0800

    init

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const run_id = core.getInput('run-id');
   console.log(run_id);
+  const repo = github.context
 
 } catch (error) {
   core.setFailed(error.message);
