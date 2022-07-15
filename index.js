69e9001560e33f50d8e1073fba859c4061d72e4d
v-ivanchen <v-ivanchen@microsoft.com>
2022-07-15 17:28:35 +0800

    reslove specifies a name that can not be retrieved problem

--- index.js
+++ index.js
@@ -19,17 +19,11 @@ const run = async () => {
     const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/attempts/${run_attempt}/jobs`)
 
     
-    // find current job id from the list of jobs
     let target = ''
-    // console.log(github.context)
-    // console.log(github)
-    // console.log(repo)
+    
     for (let job of data.jobs) {
+      // find current job id from the list of jobs
       const {data} = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/jobs/${job.id}`)
-      // console.log(data)
-      console.log('job.id: ',job.id)
-      console.log('currentjob.name: ',data.name)
-      console.log('job.name: ',job.name)
       if (data.name == job.name) {
         console.log(job.id)
         target = job.id
