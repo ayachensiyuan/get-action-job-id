40858ffcd55b6c5fac6238339d0bd3bf94edfee3
v-ivanchen <v-ivanchen@microsoft.com>
2022-07-15 16:53:25 +0800

    add test yml

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ const run = async () => {
     // find current job id from the list of jobs
     let target = ''
     // console.log(github.context)
-    console.log(github)
-    console.log(repo)
+    // console.log(github)
+    // console.log(repo)
     for (let job of data.jobs) {
       const {data} = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/jobs/${job.id}`)
       console.log(data)
       // console.log('job.id: ',job.id)
       // console.log('job.name: ',job.name)
       // console.log('github.context.job: ',github.context.job)
-      if (github.context.job == job.name) {
+      if (data.name == job.name) {
         console.log(job.id)
         target = job.id
       }
