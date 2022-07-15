a436a3bb7ad808131ab8879f5549750adb42c463
v-ivanchen <v-ivanchen@microsoft.com>
2022-07-15 09:54:07 +0800

    ready to release first version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const { Octokit } = require("@octokit/action")
-const { createActionAuth } = require("@octokit/auth-action");
+const { createActionAuth } = require("@octokit/auth-action")
 
 const run = async () => {
   try {
@@ -14,23 +14,25 @@ const run = async () => {
 
     const octokit = new Octokit({
       auth: authentication.token
-    });
+    })
 
-    const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/attempts/${run_attempt}/jobs`);
+    const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/attempts/${run_attempt}/jobs`)
 
+    // find current job id from the list of jobs
     let target = ''
     for (let job of data.jobs) {
-
       if (github.context.job == job.name) {
         console.log(job.id)
         target = job.id
       }
     }
+
+    // set id to output
     core.setOutput('jobId', JSON.stringify(target))
 
 
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 
 }
