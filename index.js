14e4457b989b42c6f806abf4c136fd353edaa5a7
= <=>
2022-09-09 00:51:18 +0800

    max cases number set to 300

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const { Octokit } = require("@octokit/action")
 const { createActionAuth } = require("@octokit/auth-action")
+const createPage = ()=>{
+
+}
 
 const run = async () => {
   try {
@@ -15,31 +18,33 @@ const run = async () => {
     const octokit = new Octokit({
       auth: authentication.token
     })
+    // default page size is 30
+    for (let page = 1; page <= 3; page++) {
+      const { data } = await octokit.request(`/repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`, {
+        page,
+        per_page: 100
+      })
 
-    const { data } = await octokit.request(`/repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
-    
-    
-    let target = ''
-    let count = 0
-    
-    for (let job of data.jobs) {
-      // find current job id from the list of jobs
-      if (job_name == job.name) {
-        console.log(job.id)
-        count++
-        target = job.id
+      let target = ''
+      let count = 0
+      
+      for (const job of data.jobs) {
+        // find current job id from the list of jobs
+        if (job_name == job.name) {
+          console.log(job.id)
+          count++
+          target = job.id
+        }
       }
+      if (count == 1)
+        // set id to output
+        core.setOutput('jobId', JSON.stringify(target))
+      else
+        core.setOutput('jobId', JSON.stringify('notUniqueId'))
     }
-    if (count == 1)
-      // set id to output
-      core.setOutput('jobId', JSON.stringify(target))
-    else
-      core.setOutput('jobId', JSON.stringify('notUniqueId'))
-
   } catch (error) {
     core.setFailed(error.message)
   }
-
 }
 
 run()
