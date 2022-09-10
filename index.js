7e1e78842cb660bfa7e42248852100cc05b14609
= <=>
2022-09-10 11:47:19 +0800

    testing

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const run = async () => {
     })
     // default page size is 30
     // for (let page = 1; page <= 3; page++) {
-      const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/per_page=100&page=1`)
+      const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs?per_page=100&page=2`)
 
       let target = ''
       let count = 0
