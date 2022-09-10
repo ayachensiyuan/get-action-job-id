ca2216c058f8aaba492e2c0e2a32e3ec5581088a
= <=>
2022-09-10 13:03:32 +0800

    fix big number jobs

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ const run = async () => {
     const octokit = new Octokit({
       auth: authentication.token
     })
-    const per_page = 10
-    // default page size is 30
-    const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
+    const per_page = 100
+    // get total number of jobs
+
+    const { data: {total_count} } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
     let count = 0
     let target = ''
-    for (let page = 1; page <= Math.floor(data.total_count/per_page) + 1; page++) {
+    for (let page = 1; page <= Math.floor(total_count/per_page) + 1; page++) {
       const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs?per_page=${per_page}&page=${page}`)
       for (const job of data.jobs) {
         // find current job id from the list of jobs
@@ -30,7 +31,6 @@ const run = async () => {
           target = job.id
         }
       }
-      console.log(count)
     }
     if (count == 1)
       // set id to output
