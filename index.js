cbeddd66d40b53b8cd2d968b5de93057cd80424b
ayachensiyuan <ayachensiyuan@163.com>
2022-10-26 09:12:01 +0800

    add debug output

--- index.js
+++ index.js
@@ -15,17 +15,19 @@ const run = async () => {
     const octokit = new Octokit({
       auth: authentication.token
     })
+    console.log('finish init env.')
     const per_page = 100
     // get total number of jobs
     const { data: {total_count} } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
     let count = 0
     let target = ''
+    console.log('total job: ', total_count)
     for (let page = 1; page <= Math.floor(total_count/per_page) + 1; page++) {
       const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs?per_page=${per_page}&page=${page}`)
       for (const job of data.jobs) {
         // find current job id from the list of jobs
         if (job_name === job.name) {
-          console.log(job.id)
+          console.log('find match id is: ', job.id)
           count++
           target = job.id
         }
@@ -37,6 +39,7 @@ const run = async () => {
     else
       core.setOutput('jobId', JSON.stringify('notUniqueId'))
   } catch (error) {
+    console.log(error)
     core.setFailed(error.message)
   }
 }
