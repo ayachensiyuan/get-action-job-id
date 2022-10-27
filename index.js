ac8811882ade02d4dd707ff77ba96934199637b8
ayachensiyuan <ayachensiyuan@163.com>
2022-10-27 11:04:04 +0800

    add retry

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ const { Octokit } = require("@octokit/core");
 const { retry } = require("@octokit/plugin-retry");
 const { createActionAuth } = require("@octokit/auth-action")
 
+async function getId(run_id, repo, per_page) {
+  const { data: { total_count } } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
+  let count = 0
+  let target = ''
+  console.log('total job: ', total_count)
+  for (let page = 1; page <= Math.floor(total_count / per_page) + 1; page++) {
+    const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs?per_page=${per_page}&page=${page}`)
+    for (const job of data.jobs) {
+      // find current job id from the list of jobs
+      if (job_name === job.name) {
+        console.log('find match id is: ', job.id)
+        count++
+        target = job.id
+      }
+    }
+  }
+  if (count === 1)
+    return target
+  else
+    return -1
+}
+
+
+
+
 const run = async () => {
   try {
     const { repo, runId: run_id } = github.context
@@ -21,34 +46,27 @@ const run = async () => {
     const per_page = 100
     // get total number of jobs
     try {
-      const { data: {total_count} } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
-      let count = 0
-      let target = ''
-      console.log('total job: ', total_count)
-      for (let page = 1; page <= Math.floor(total_count/per_page) + 1; page++) {
-        const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs?per_page=${per_page}&page=${page}`)
-        for (const job of data.jobs) {
-          // find current job id from the list of jobs
-          if (job_name === job.name) {
-            console.log('find match id is: ', job.id)
-            count++
-            target = job.id
-          }
-        }
-      }
-      if (count == 1)
+      const target = await getId(run_id, repo, per_page)
+      if (target === -1)
+        core.setOutput('jobId', JSON.stringify('notUniqueId'))
+      else
         // set id to output
         core.setOutput('jobId', JSON.stringify(target))
-      else
-        core.setOutput('jobId', JSON.stringify('notUniqueId'))
     } catch (error) {
       if (error.request.request.retryCount) {
+        console.log(error)
         console.log(
           `request failed after ${error.request.request.retryCount} retries`
         );
+        
+        // try again
+        const target = await getId(run_id, repo, per_page)
+        if (target === -1)
+          core.setOutput('jobId', JSON.stringify('notUniqueId'))
+        else
+          // set id to output
+          core.setOutput('jobId', JSON.stringify(target))
       }
-    
-      console.error(error);
     }
   } catch (error) {
     console.log(error)
