97b3485868dc1ff6ad59518e0b2e25dd639afcd1
ayachensiyuan <ayachensiyuan@163.com>
2022-10-27 10:48:27 +0800

    add retry

--- index.js
+++ index.js
@@ -15,32 +15,41 @@ const run = async () => {
 
     const MyOctokit = Octokit.plugin(retry);
     const octokit = new MyOctokit({
-      auth: authentication.token,
-      request: {retry: 5, retryAfter: 10 }
+      auth: authentication.token
     })
     console.log('finish init env.')
     const per_page = 100
     // get total number of jobs
-    const { data: {total_count} } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
-    let count = 0
-    let target = ''
-    console.log('total job: ', total_count)
-    for (let page = 1; page <= Math.floor(total_count/per_page) + 1; page++) {
-      const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs?per_page=${per_page}&page=${page}`)
-      for (const job of data.jobs) {
-        // find current job id from the list of jobs
-        if (job_name === job.name) {
-          console.log('find match id is: ', job.id)
-          count++
-          target = job.id
+    try {
+      const { data: {total_count} } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs`)
+      let count = 0
+      let target = ''
+      console.log('total job: ', total_count)
+      for (let page = 1; page <= Math.floor(total_count/per_page) + 1; page++) {
+        const { data } = await octokit.request(`GET /repos/${repo.owner}/${repo.repo}/actions/runs/${run_id}/jobs?per_page=${per_page}&page=${page}`)
+        for (const job of data.jobs) {
+          // find current job id from the list of jobs
+          if (job_name === job.name) {
+            console.log('find match id is: ', job.id)
+            count++
+            target = job.id
+          }
         }
       }
+      if (count == 1)
+        // set id to output
+        core.setOutput('jobId', JSON.stringify(target))
+      else
+        core.setOutput('jobId', JSON.stringify('notUniqueId'))
+    } catch (error) {
+      if (error.request.request.retryCount) {
+        console.log(
+          `request failed after ${error.request.request.retryCount} retries`
+        );
+      }
+    
+      console.error(error);
     }
-    if (count == 1)
-      // set id to output
-      core.setOutput('jobId', JSON.stringify(target))
-    else
-      core.setOutput('jobId', JSON.stringify('notUniqueId'))
   } catch (error) {
     console.log(error)
     core.setFailed(error.message)
